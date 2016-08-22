cc0a95a12546fb2fce5ff6b7fe53d7b205268005
Christoph Zauner <christoph.zauner@NLLK.net>
2016-08-22 06:54:42 +0000

    Add cli package.

--- cntinsight.go
+++ cntinsight.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"bufio"
-        "bytes"
+	"bytes"
 	"fmt"
 	"html/template"
-        "io"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
 
-        "github.com/shurcooL/github_flavored_markdown"
+	"github.com/shurcooL/github_flavored_markdown"
 	"github.com/shurcooL/github_flavored_markdown/gfmstyle"
-        //"github.com/russross/blackfriday"
+	//"github.com/russross/blackfriday"
+	"github.com/urfave/cli"
 )
 
 type Page struct {
@@ -33,35 +34,34 @@ func convertMdToHtml(pathToFile string) (*Page, error) {
 
 	pageAsMarkdown, error := ioutil.ReadFile(pathToFile)
 
+	appendix := enrichMd(getReadmeAsMarkdown())
+	// Make sure that there is at least one newline before our heading
+	pageAsMarkdown = append(pageAsMarkdown, "\n"...)
+	for _, appendixLine := range appendix {
+		pageAsMarkdown = append(pageAsMarkdown, appendixLine+"\n"...)
+	}
 
-        appendix := enrichMd(getReadmeAsMarkdown())
-        // Make sure that there is at least one newline before our heading
-        pageAsMarkdown = append(pageAsMarkdown, "\n"...)
-        for _, appendixLine := range appendix {
-            pageAsMarkdown = append(pageAsMarkdown, appendixLine + "\n"...)
-        }
-        
-        pageAsHtml := github_flavored_markdown.Markdown(pageAsMarkdown)
+	pageAsHtml := github_flavored_markdown.Markdown(pageAsMarkdown)
 
 	if error != nil {
 		return nil, error
 	}
-        
+
 	return &Page{Path: pathToFile, Markdown: pageAsMarkdown, Html: pageAsHtml}, nil
 }
 
 /**
  * Adds various informations to the README.md of the container.
  */
-func enrichMd(rdmeAsMdArr []string) (enrichedRdmeAsMdArr []string) { 
-    
+func enrichMd(rdmeAsMdArr []string) (enrichedRdmeAsMdArr []string) {
+
 	appendixTemplate, error := template.ParseFiles("appendix_template.md")
-        var doc bytes.Buffer 
-        error = appendixTemplate.Execute(&doc, nil)
-        s := doc.String()
-        
+	var doc bytes.Buffer
+	error = appendixTemplate.Execute(&doc, nil)
+	s := doc.String()
+
 	if error != nil {
-                fmt.Printf("Error while processing template: >%s<.", error)
+		fmt.Printf("Error while processing template: >%s<.", error)
 		return rdmeAsMdArr
 	}
 
@@ -128,13 +128,29 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/log", logHandler)
-        
-        // Serve the "/assets/gfm.css" file.
-        http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(gfmstyle.Assets)))
-        
-	http.ListenAndServe(":8080", nil)
+
+	app := cli.NewApp()
+
+	app.Email = "christoph.zauner@NLLK.net"
+	app.Author = "Christoph Zauner"
+	app.Version = "0.1.0-alpha"
+	// cntrinfod, cntinfod
+	app.Usage = "Container Insight: HTTP daemon which exposes and augments the containers REAMDE.md"
+
+	app.Action = func(c *cli.Context) error {
+
+		http.HandleFunc("/", handler)
+		http.HandleFunc("/log", logHandler)
+
+		// Serve the "/assets/gfm.css" file.
+		http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(gfmstyle.Assets)))
+
+		http.ListenAndServe(":8080", nil)
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
 
 func getReadmeAsMarkdown() []string {
@@ -150,8 +166,8 @@ func getReadmeAsMarkdown() []string {
 			break
 		}
 	}
-        defer fd.Close()
-	
+	defer fd.Close()
+
 	if err == nil {
 		fmt.Printf("Could not find README.md file.")
 		os.Exit(-1)
@@ -159,19 +175,19 @@ func getReadmeAsMarkdown() []string {
 
 	var lines []string
 	scanner := bufio.NewScanner(fd)
-        for scanner.Scan() {
-            lines = append(lines, scanner.Text())
-        }
-	
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
 	return lines
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	page, _ := convertMdToHtml("testdata/README.md")
-        
-        io.WriteString(w, `<html><head><meta charset="utf-8"><link href="/assets/gfm.css" media="all" rel="stylesheet" type="text/css" /><link href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;">`)
-        w.Write(page.Html)
-        io.WriteString(w, `</article></body></html>`)
+
+	io.WriteString(w, `<html><head><meta charset="utf-8"><link href="/assets/gfm.css" media="all" rel="stylesheet" type="text/css" /><link href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css" media="all" rel="stylesheet" type="text/css" /></head><body><article class="markdown-body entry-content" style="padding: 30px;">`)
+	w.Write(page.Html)
+	io.WriteString(w, `</article></body></html>`)
 }
 
 func logHandler(w http.ResponseWriter, r *http.Request) {

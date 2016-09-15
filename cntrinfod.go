f592d8370f55bffdac91343f00a1f2a18f473fca
Christoph Zauner <christoph.zauner@NLLK.net>
2016-09-15 13:56:53 +0000

    Release version 0.2.3.

--- cntrinfod.go
+++ cntrinfod.go
@@ -172,7 +172,7 @@ func main() {
 
 	app.Email = "christoph.zauner@NLLK.net"
 	app.Author = "Christoph Zauner"
-	app.Version = "0.2.3-SNAPSHOT"
+	app.Version = "0.2.3"
 	// cntrinfod, cntinfod
 	app.Usage = "Container Info Daemon: HTTP daemon which exposes and augments the containers REAMDE.md"
 
